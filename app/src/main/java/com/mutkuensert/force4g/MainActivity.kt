37249f7762a2f6445945fdc4653c6afe29f347ba
Mustafa Utku Ensert <mutkuensert@hotmail.com>
2022-08-20 13:10:49 +0300

    Works with Android 11 and below.

--- app/src/main/java/com/mutkuensert/force4g/MainActivity.kt
+++ app/src/main/java/com/mutkuensert/force4g/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mutkuensert.force4g
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,10 +11,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val androidOS = Build.VERSION.SDK_INT
+
         try {
             val intent = Intent(Intent.ACTION_MAIN)
-            intent.setClassName("com.android.phone", "com.android.phone.settings.RadioInfo")
+
+            if(androidOS >= 30 ){// https://stackoverflow.com/a/66165805
+                intent.setClassName("com.android.phone", "com.android.phone.settings.RadioInfo") //https://stackoverflow.com/a/50409322
+
+            }else{
+                intent.setClassName("com.android.settings", "com.android.settings.RadioInfo")
+            }
+
             startActivity(intent)
+
         } catch (e: Exception) {
             Toast.makeText(applicationContext, " Device not supported", Toast.LENGTH_LONG).show()
         }
